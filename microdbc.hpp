caf86bd7c171a39d4a6d6d358c158e61e8910165
Fatih AKTAŞ <132843565+fatihaktas845@users.noreply.github.com>
2023-12-25 22:38:39 +0300

    Microdbc Header File

---
+++ microdbc.hpp
@@ -0,0 +1,31 @@
+#ifndef MICRODBC_HPP
+#define MICRODBC_HPP
+
+#include <windows.h>
+#include <iostream>
+#include <sql.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+
+class Microdbc
+{
+private:
+	SQLHANDLE sqlEnvHandle;
+	SQLHANDLE sqlConnectionHandle;
+	SQLHANDLE sqlStatementHandle;
+	SQLRETURN retcode;
+	
+	std::string connString;
+	
+	void print_error(unsigned int handleType, const SQLHANDLE& handle);
+	
+public:
+	Microdbc();
+	~Microdbc();
+	
+	bool connect(const std::string& connectionString);
+	void disconnect();
+	std::string executeQuery(const std::string query);
+};
+
+#endif // MICRODBC_HPP
